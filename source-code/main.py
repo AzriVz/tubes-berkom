43e9bff091113f5f8cb87b80d8e547de4cd30688
Azri <93464242+AzriVz@users.noreply.github.com>
2024-11-18 04:50:53 +0700

    Update main.py

--- source-code/main.py
+++ source-code/main.py
@@ -141,8 +141,7 @@ def input_jenis_kendaraan():
             if 1 <= jenis <= 5:
                 return jenis
             else:
-                print("    Mohon maaf anda tidak dapat lewat melalui jalur ini")
-                break
+                print("   Golongan kendaraan harus antara 1 dan 5. Silakan pilih ulang.")
         except ValueError:
             print("    Input tidak valid. Harap masukkan angka yang sesuai daftar.")
 
